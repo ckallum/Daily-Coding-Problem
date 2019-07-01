6dbbb6d5a86452a1347fd539ad7c5ebc2760b4e3
ckallum <in18536@bristol.ac.uk>
2019-07-01 20:43:41 +0100

    Done

---
+++ firstMissingInt.py
@@ -0,0 +1,21 @@
+def findMissingBrute(l):
+    if len(l) == 0:
+        return 1
+    nextInt = 1
+    found = False
+    while not(found):
+        if nextInt in l:
+            nextInt += 1
+        else:
+            found = True
+
+    return nextInt
+
+
+def main():
+    nums = [3, 4, -1, 1, 0, 3, 4, 6, 7, 8, 3, 2, 10]
+    print(findMissingBrute(nums))
+
+
+if __name__ == '__main__':
+    main()
