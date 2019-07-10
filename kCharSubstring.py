70e363b55cc772d37872f1f25084563e0528951e
ckallum <ckallumke@gmail.com>
2019-07-10 23:16:27 +0100

    Done

--- kCharSubstring.py
+++ kCharSubstring.py
@@ -1,28 +1,51 @@
-import string
+# Maxima window problem
+
+from string import ascii_lowercase
 
 
-def solution(string, k):
-    charCountDict = dict.fromkeys(string.ascii_lowercase, 0)
+def isVal(dic, k):
+    count = 0
+    for x in dic:
+        if dic[x] != 0:
+            count += 1
+    return k >= count
+
+
+def solution(s, k):
+    charCountDict = dict.fromkeys(ascii_lowercase, 0)
     distinctChars = 0
-    returnString = ''
-    maxLength = 0
+    windowStartIndex = 0
+    windowEndIndex = 0
+    maxStartIndex = 0
+    maxSize = 1
 
-    for x in string:
+    for x in s:
         if charCountDict[x] == 0:
             distinctChars += 1
         charCountDict[x] += 1
 
-        if k > distinctChars:
-            print('Not enough distinct chararcters')
-            return
+    if k > distinctChars:
+        print('Not enough distinct characters')
+        return
 
-        # Algorithmic window problem
+    charCountDict = dict.fromkeys(ascii_lowercase, 0)
+    charCountDict[s[windowStartIndex]] += 1
+    for x in s[1:]:
+        windowEndIndex += 1
+        charCountDict[x] += 1
+        while not isVal(charCountDict, k):
+            charCountDict[s[windowStartIndex]] -= 1
+            windowStartIndex += 1
 
-    return returnString, maxLength
+        if windowEndIndex - windowStartIndex + 1 > maxSize:
+            maxSize = windowEndIndex - windowStartIndex + 1
+            maxStartIndex = windowStartIndex
+
+    return maxSize, s[maxStartIndex: maxStartIndex + maxSize]
 
 
 def main():
-    x, y = solution('abcbbbadadadadada', 3)
+    y, x = solution("abcbbbadadadadada", 3)
     print('Result:', x, 'Length:', y)
 
 
