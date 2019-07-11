2694e6fa17c1b870eb25aa3b230035f6699e3d93
ckallum <ckallumke@gmail.com>
2019-07-11 20:41:09 +0100

    hi

---
+++ piEstimator.py
@@ -0,0 +1,26 @@
+from random import randrange
+
+
+def estimate():
+    sigFigs = 1000
+    inCircle = 0
+    inSquare = 0
+    for _ in range(sigFigs * sigFigs):
+        randomX = randrange(sigFigs)/ sigFigs
+        randomY = randrange(sigFigs)/ sigFigs
+        rSquared = (randomX**2) + (randomY **2)
+        if rSquared <= 1:
+            inCircle += 1
+        inSquare += 1
+
+    pi = 4*(inCircle/inSquare)
+    print(pi)
+
+
+
+def main():
+    estimate()
+
+
+if __name__ == '__main__':
+    main()
