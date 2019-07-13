4c6f372de1d80feaf44aa2f2056d62cb982a5a7b
ckallum <ckallumke@gmail.com>
2019-07-13 19:30:17 +0100

    Done

---
+++ logRecorder.py
@@ -0,0 +1,32 @@
+class Log(object):
+    def __init__(self, size):
+        self.log = list()
+        self.size = size
+
+    def __repr__(self):
+        return str(self.log)
+
+    def record(self, order_id):
+        self.log.append(order_id)
+        if len(self.log) > self.size:
+            self.log = self.log[1:]
+
+    def getLast(self, i):
+        return self.log[-i]
+
+
+def main():
+    log = Log(5)
+    for x in range(10):
+        log.record(x)
+        print(repr(log))
+
+    assert log.getLast(1) == 9
+    assert log.getLast(2) == 8
+    assert log.getLast(3) == 7
+    assert log.getLast(4) == 6
+    assert log.getLast(5) == 5
+
+
+if __name__ == '__main__':
+    main()
