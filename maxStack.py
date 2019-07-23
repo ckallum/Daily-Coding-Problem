8f46606764922cd011ffcc3b53442f0cd9432810
ckallum <ckallumke@gmail.com>
2019-07-23 20:09:43 +0100

    Done

--- maxStack.py
+++ maxStack.py
@@ -20,18 +20,28 @@ class UnderFlowException(Exception):
 class MaxStack:
     def __init__(self, size):
         self.stack = []
-        self.size = size
-        self.max = -maxsize
+        self.size = 0
+        self.maxLen = size
+        self.m = None
 
     def push(self, val):
-        if self.size == len(self.stack):
+        if self.size == self.maxLen:
             try:
                 raise OverFlowException()
             except OverFlowException as e:
                 print(e)
-        if val > self.max:
-            self.stack.append(2 * val - self.max)
-            self.max = val
+        if self.size == 0:
+            self.m = val
+            self.size += 1
+            self.stack.append(val)
+
+        elif val > self.m:
+            self.stack.append(2 * val - self.m)
+            self.size += 1
+            self.m = val
+        else:
+            self.stack.append(val)
+            self.size += 1
 
     def pop(self):
         if self.size == 0:
@@ -39,23 +49,34 @@ class MaxStack:
                 raise UnderFlowException()
             except UnderFlowException as e:
                 print(e)
-        val = self.stack.pop(self.size-1)
-        if val == self.max:
-            self.max = 2*self.max - val
+        val = self.stack.pop(self.size - 1)
+        self.size -= 1
+        if val > self.m:
+            preV = 2*self.m-val
+            self.m = preV
 
     def max(self):
-        return self.max
+        return self.m
+
+    def __repr__(self):
+
+        return "{}".format(", ".join(str(v) for v in self.stack))
 
 
 def main():
-    s = MaxStack()
+    s = MaxStack(4)
     s.push(1)
+    print(repr(s))
     s.push(2)
+    print(repr(s))
     s.push(3)
+    print(repr(s))
     s.push(2)
+    print(repr(s))
     print(s.max())
     # 3
     s.pop()
+    print(s.max())
     s.pop()
     print(s.max())
 
