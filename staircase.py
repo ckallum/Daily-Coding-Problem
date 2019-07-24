f85bb26ce360c556bae9d4d401efeb161a4763db
ckallum <ckallumke@gmail.com>
2019-07-24 17:38:16 +0100

    Done

--- staircase.py
+++ staircase.py
@@ -12,22 +12,22 @@ def steps(n):
 
 
 def variedSteps(n, lst):
-    stepsDict = [0 for _ in range(n + 1)]
+    stepsDict = dict()
     stepsDict[0] = 1
+    if n < min(lst):
+        return 0
     for x in range(1, n + 1):
-        if x < min(lst):
-            stepsDict[x] = 0
-        else:
-            for step in lst:
-                if x >= step and stepsDict[x - step] != 0:
-                    stepsDict[x] += stepsDict[x - step]
+        stepsDict[x] = 0
+        for step in lst:
+            if x - step in stepsDict:
+                stepsDict[x] += stepsDict[x - step]
     return stepsDict[n]
 
 
 def main():
-    i = input('How many steps')
-    print(steps(i))
-    print(variedSteps(i, [1, 2, 3]))
+    assert variedSteps(4, [1, 2]) == 5
+    assert variedSteps(5, [1, 2, 3]) == 13
+    assert steps(4) == 5
 
 
 if __name__ == '__main__':
