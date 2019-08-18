fc20f3518fb1cb6211f630d2f074e07087388103
ckallum <ckallumke@gmail.com>
2019-08-18 04:52:34 +0100

    Done

--- removeConsecutiveNodes.py
+++ removeConsecutiveNodes.py
@@ -6,33 +6,49 @@ class Node:
 
 def removeConsecutiveSumTo0(node):
     start = node
-    end = None
+    head = Node(None)
+    head.next = start
+    headpointer = head
     while start:
         removed = False
         total = 0
         end = start
+        increment = 0
         while end:
             total += end.value
             if total == 0:
                 removed = True
-                start = end
                 break
             else:
                 end = end.next
-
+                increment += 1
         if removed:
-            
+            temp = head.next
+            for _ in range(increment):
+                temp = head.next
+                start = start.next
+            if temp is not None:
+                head.next = Node(temp.value)
+                head = head.next
+            else:
+                head.next = None
+        start = start.next
+    return headpointer.next
 
 
-node = Node(10)
-node.next = Node(5)
-node.next.next = Node(-3)
-node.next.next.next = Node(-3)
-node.next.next.next.next = Node(1)
-node.next.next.next.next.next = Node(4)
-node.next.next.next.next.next.next = Node(-4)
-node = removeConsecutiveSumTo0(node)
-while node:
-    print(node.value)
-    node = node.next
-# 10
+def main():
+    node = Node(10)
+    node.next = Node(-10)
+    node.next.next = Node(-3)
+    node.next.next.next = Node(-3)
+    node.next.next.next.next = Node(1)
+    node.next.next.next.next.next = Node(4)
+    node.next.next.next.next.next.next = Node(-4)
+    node = removeConsecutiveSumTo0(node)
+    while node:
+        print(node.value)
+        node = node.next
+
+
+if __name__ == '__main__':
+    main()
