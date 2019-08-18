94683dfe54d515d2e1f5b6348fe14b14524c4c56
ckallum <ckallumke@gmail.com>
2019-08-18 10:25:14 +0100

    Done

--- shuffledeck.py
+++ shuffledeck.py
@@ -1,10 +1,21 @@
+
+
+"""
+Proof: Each element's final position from positions n-1 to 0 has a probability of 1/(it's index) =
+1/n * 1/n-1 ... 1/1 = 1/(n!)
+which means the total probability of a single permutation = 1/(n!) which is correct as there is a total of
+n! permutations so each permutation should have equal probability/
+
+"""
+
+
 from random import randint
 
 
 def shuffle():
     cards = list(range(52))
-    for i in range(52):
-        newindex = i + randint(0, 52 - i - 1)
+    for i in range(52, 0, -1):
+        newindex = randint(0, i+1)
         temp = cards[i]
         cards[i] = cards[newindex]
         cards[newindex] = temp
