386e96226c0d66a4d951e9f6d7e6c37244977195
ckallum <ckallumke@gmail.com>
2019-08-22 03:09:21 +0100

    Done

--- lookandsay.py
+++ lookandsay.py
@@ -4,8 +4,6 @@ from collections import deque
 def lookandsay(n):
     if n == 1:
         return [1]
-    if n == 2:
-        return [1, 1]
     current = [1]
     for _ in range(2, n + 1):
         temp = list()
@@ -26,6 +24,9 @@ def lookandsay(n):
 
 
 def main():
+    assert lookandsay(2) == [1, 1]
+    assert lookandsay(3) == [2, 1]
+    assert lookandsay(4) == [1, 2, 1, 1]
     assert lookandsay(5) == [1, 1, 1, 2, 2, 1]
 
 
