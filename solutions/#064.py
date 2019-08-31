2d63f1b6f3078a41013e8fc078640dc414497b44
ckallum <ckallumke@gmail.com>
2019-08-31 10:49:06 +0100

    Done

--- solutions/#064.py
+++ solutions/#064.py
@@ -1,6 +1,6 @@
-def knights_tour(n, board, current_row, current_col):
+def knights_tour(n, visited, current_row, current_col):
     count = 0
-    if is_complete(board, n):
+    if is_complete(visited, n):
         return 1
 
     possible_moves = [(current_row - 1, current_col - 2), (current_row - 1, current_col + 2),
@@ -8,40 +8,43 @@ def knights_tour(n, board, current_row, current_col):
                       (current_row + 1, current_col - 2), (current_row + 1, current_col + 2),
                       (current_row + 2, current_col - 1), (current_row + 2, current_col + 1)]
     for move in possible_moves:
-        if is_valid(move[0], move[1], board, n):
-            board[move[0]][move[1]] = True
-            count += knights_tour(n, board, move[0], move[1])
-            board[move[0]][move[1]] = False
-
+        if is_valid(move[0], move[1], visited, n):
+            visited.append(move)
+            count += knights_tour(n, visited, move[0], move[1])
+            visited.pop()
     return count
 
 
 def knights_tour_start(n):
     count = 0
-    board = [[False for _ in range(n)] for _ in range(n)]
+    visited = []
     for row in range(n):
         for col in range(n):
-            board[row][col] = True
-            count += knights_tour(n, board, row, col)
-            board[row][col] = False
+            visited.append((row, col)
+                           )
+            count += knights_tour(n, visited, row, col)
+            visited.pop()
     return count
 
 
-def is_complete(board, n):
-    return n * n == len([(x, y) for x in range(n) for y in range(n) if board[x][y]])
+def is_complete(visited, n):
+    return n * n == len(visited)
 
 
-def is_valid(row, col, board, n):
+def is_valid(row, col, visited, n):
     if row < 0 or col < 0:
         return False
     elif row >= n or col >= n:
         return False
     else:
-        return not board[row][col]
+        return not ((row, col) in visited)
 
 
 def main():
-    print(knights_tour_start(5))
+    assert knights_tour_start(1) == 1
+    assert knights_tour_start(2) == 0
+    assert knights_tour_start(3) == 0
+    assert knights_tour_start(4) == 0
 
 
 if __name__ == '__main__':
