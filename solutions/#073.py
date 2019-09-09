e6fe33f25b9dffafb8ccd3dcd6be687b22c03dbf
ckallum <ckallumke@gmail.com>
2019-09-09 15:41:46 +0100

    Done

--- solutions/#073.py
+++ solutions/#073.py
@@ -13,15 +13,30 @@ def print_list(node):
     return value_list
 
 
-def reverse_linked_list(head):
+def recursive_reverse_linked_list(head):
     if not head:
         return None
     if not head.next:
         return head
-    next_node = reverse_linked_list(head.next)
+    next_node = recursive_reverse_linked_list(head.next)
     next_node.next = head
     head.next = None
     return next_node.next
+
+
+def iterative_reverse_linked_list(head):
+    if not head.next:
+        return head
+    head_next = head.next
+    head.next = None
+    next_next = head_next.next
+    while head_next:
+        head_next.next = head
+        head = head_next
+        head_next = next_next
+        if head_next:
+            next_next = head_next.next
+    return head
 
 
 def main():
@@ -32,8 +47,17 @@ def main():
     root.next = two
     two.next = three
     three.next = four
-    reverse_linked_list(root)
+    recursive_reverse_linked_list(root)
     assert print_list(four) == [4, 3, 2, 1]
+    root = Node(1)
+    two = Node(2)
+    three = Node(3)
+    four = Node(4)
+    root.next = two
+    two.next = three
+    three.next = four
+    head = iterative_reverse_linked_list(root)
+    assert print_list(head) == [4, 3, 2, 1]
 
 
 if __name__ == '__main__':
