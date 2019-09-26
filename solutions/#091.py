d5ad860d332c69e05815ae604e1d7dc671c8b387
ckallum <in18536@bristol.ac.uk>
2019-09-26 19:34:04 +0100

    Done

---
+++ solutions/#091.py
@@ -0,0 +1,34 @@
+# Function is broken because the function get's passed in the current local value of i which is fixed at 9
+
+
+def faulty_function():
+    functions = []
+    result = []
+    for i in range(10):
+        functions.append(lambda: i)
+
+    for f in functions:
+        result.append(f())
+    return result
+
+
+def fixed_function():
+    functions = []
+    result = []
+    for i in range(10):
+        functions.append(lambda: i)
+    i = 0
+    for f in functions:
+        result.append(f())
+        i += 1
+
+    return result
+
+
+def main():
+    assert faulty_function() == [9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+    assert fixed_function() == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+if __name__ == '__main__':
+    main()
