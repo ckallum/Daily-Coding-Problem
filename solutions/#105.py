cd9d1cbb770fd15698a2bfecd7ad07ceff7bf6dc
ckallum <in18536@bristol.ac.uk>
2019-10-12 18:25:05 +0100

    Done

--- solutions/#105.py
+++ solutions/#105.py
@@ -5,21 +5,24 @@ def debounce(s):
     """
     Decorator ensures function that can only be called once every `s` seconds.
     """
-    interval = s * (10**(-3))
+    interval = s * (10 ** (-3))
 
     def decorate(f):
         current_time = None
+
         # A decorator allows us to take in a function f and wrap it in another function i.e. changing its behaviour
         # without modifying the function f itself.
         def wrapped(*args, **kwargs):
-            #nonlocal changes the current_time in the outer function as long as it isn't global
+            # nonlocal changes the current_time in the outer function as long as it isn't global
             nonlocal current_time
             start_time = time.time()
             if current_time is None or start_time - current_time >= interval:
                 result = f(*args, **kwargs)
                 current_time = time.time()
                 return result
+
         return wrapped
+
     return decorate
 
 
@@ -27,6 +30,7 @@ def debounce(s):
 # '@' function is used to decorate a function, i.e. wrap it into something else when it is called.
 def add_nums(x, y):
     return x + y
+
 
 def main():
     assert add_nums(1, 1) == 2
@@ -39,3 +43,4 @@ def main():
 
 
 if __name__ == '__main__':
+    main()
