6ec90f9caad657e7fcf37ffc72da9499db49cc97
ckallum <in18536@bristol.ac.uk>
2019-11-12 19:28:37 +0000

    Done

--- solutions/#138.py
+++ solutions/#138.py
@@ -2,7 +2,7 @@ import heapq
 from cmath import inf
 
 
-def min_count(n):
+def n_logn_min_count(n):
     if not n:
         return 0
     count = 0
@@ -18,6 +18,18 @@ def min_count(n):
         n -= current
         count += 1
     return count
+
+
+def min_count(n):
+    coin_dict = [inf for _ in range(n + 1)]
+    coin_dict[0] = 0
+    coins = [1, 25, 10, 5]
+    for i in range(1, n + 1):
+        for j in range(len(coins)):
+            if coins[j] <= i and coin_dict[i-coins[j]] != inf:
+                coin_dict[i] = min(1 + coin_dict[i-coins[j]], coin_dict[i])
+    print(coin_dict[n])
+    return coin_dict[n]
 
 
 def main():
