58cce5817dc9e9f764d711bb57dd56a0c38da621
ckallum <in18536@bristol.ac.uk>
2019-12-07 18:47:03 +0000

    Done

---
+++ solutions/#163.py
@@ -0,0 +1,25 @@
+def reverse_polish(equation):
+    num_stack = list()
+    operations = {"*": lambda x, y: x * y,
+                  "/": lambda x, y: x / y,
+                  "-": lambda x, y: x - y,
+                  "+": lambda x, y: x + y}
+    result = 0
+
+    for char in equation:
+        if char in operations:
+            if len(num_stack) < 2:
+                return None
+            res = operations[char](num_stack.pop(), num_stack.pop())
+            num_stack.append(res)
+        else:
+            num_stack.append(char)
+    return num_stack.pop()
+
+
+def main():
+    assert reverse_polish([15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-']) == 5
+
+
+if __name__ == '__main__':
+    main()
