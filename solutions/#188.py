a983429a09047ce72fcec5e21f857384c407d569
ckallum <ckallumke@gmail.com>
2020-01-01 20:45:25 +0000

    Done

---
+++ solutions/#188.py
@@ -0,0 +1,15 @@
+def make_functions():
+    flist = []
+
+    for i in [1, 2, 3]:
+        def print_i(x):
+            print(x)
+
+        flist.append((print_i, i))
+
+    return flist
+
+
+functions = make_functions()
+for f, i in functions:
+    f(i)
