7c00d2cda8734533fef5759c96034cb82a1b7758
ckallum <in18536@bristol.ac.uk>
2020-01-13 21:17:35 +0000

    Done

--- solutions/#200.py
+++ solutions/#200.py
@@ -1,14 +1,14 @@
 def interval_stab(intervals):
-    intervals = sorted(intervals, key=lambda x: x[0])
-    result = []
+    intervals = sorted(intervals, key=lambda x: x[1])
+    result = set()
     for start, end in intervals:
-        if not result or start != result[-1]:
-            result.append(end)
-    return result
+        if not result or start not in result:
+            result.add(end)
+    return list(result)
 
 
 def main():
-    assert interval_stab([(1, 4), (4, 5), (7, 9), (9, 12)]) == [4, 9]
+    assert interval_stab([(1, 4), (4, 5), (7, 9), (9, 12)]) == [9, 4]
 
 
 if __name__ == '__main__':
