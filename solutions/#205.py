9c238b8f79c181e4f4e1da0e136275fb7fe04ab8
ckallum <in18536@bristol.ac.uk>
2020-01-18 20:07:41 +0000

    Done

--- solutions/#205.py
+++ solutions/#205.py
@@ -2,20 +2,19 @@ from bisect import bisect
 
 
 def next_lexographic_permutation(number):
-    number = str(number)
+    number = list(str(number))
     i = 0
-    right = []
     replaced = None
     for i in range(len(number) - 2, 0, -1):
-        right.append(number[i + 1])
         if number[i] < number[i + 1]:
             replaced = number[i]
             break
-    replaced_index = bisect(right, replaced)
-    replacement = right[replaced_index]
-    right[replaced_index] = replaced
-    return int(number[:i]+replacement+"".join(sorted(right)))
-
+    for j in range(len(number) - 1, i, -1):
+        if number[j] > replaced:
+            number[i], number[j] = number[j], number[i]
+            break
+    number[i + 1:] = reversed(number[i + 1:])
+    return int("".join(number))
 
 
 def main():
