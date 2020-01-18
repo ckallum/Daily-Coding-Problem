24969285a4259b419cf03a7b017f8d72e3845e1d
ckallum <in18536@bristol.ac.uk>
2020-01-18 20:00:04 +0000

    Done

---
+++ solutions/#205.py
@@ -0,0 +1,26 @@
+from bisect import bisect
+
+
+def next_lexographic_permutation(number):
+    number = str(number)
+    i = 0
+    right = []
+    replaced = None
+    for i in range(len(number) - 2, 0, -1):
+        right.append(number[i + 1])
+        if number[i] < number[i + 1]:
+            replaced = number[i]
+            break
+    replaced_index = bisect(right, replaced)
+    replacement = right[replaced_index]
+    right[replaced_index] = replaced
+    return int(number[:i]+replacement+"".join(right))
+
+
+
+def main():
+    assert next_lexographic_permutation(48975) == 49578
+
+
+if __name__ == '__main__':
+    main()
