0956c100b4b6b8e3dfe8fab767593b64cd60c9c2
ckallum <in18536@bristol.ac.uk>
2020-01-18 20:13:09 +0000

    Done

--- solutions/#205.py
+++ solutions/#205.py
@@ -1,4 +1,13 @@
-from bisect import bisect
+"""
+Goal is to find the first available unit's digit that can be replaced by a units digit lower than it -> i.e. smallest
+possible increase in number = next lexographic permutation
+We then find the next largest number in the previous digits to replace -> swap the numbers.
+We then reverse the number from the replaced units index+1 of the number(i.e. reverse the tail)
+    Currently the tail is guaranteed to be in descending order -> reversing the tail is the smallest increase possible
+    as  we have already guaranteed a larger number by swapping the identified unit->goal is to find smallest possible
+    increase.
+        i.e 6215430 -> swap 1 and 3-> 6235410 ->reverse from swapped index onward(reverse 0145)-> 6230145
+"""
 
 
 def next_lexographic_permutation(number):
